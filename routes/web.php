44eaf3647154b04985b7f793a051b06b393a96a7
Fathir <fathirulfatih29@gmail.com>
2025-10-14 11:23:07 +0700

    change welcome to index

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
-
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BlogController::class, 'index'])->name('home');
 
 //http method 
 
