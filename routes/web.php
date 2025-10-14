09446ad1612839397ec310c378c5fd8d5c0883a0
Fathir <fathirulfatih29@gmail.com>
2025-10-14 10:48:09 +0700

    add content prefix

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,17 @@ Route::put('/update', function(){
 Route::delete('/delete', function(){
     return 'data berhasil di update';
 });
+
+//grub prefix
+
+use App\Http\Controllers\BlogController;
+
+// 🔹 ROUTE GROUP PREFIX
+Route::prefix('blog')->group(function () {
+    Route::get('/', [BlogController::class, 'index'])->name('blog.index');
+    Route::get('/create', [BlogController::class, 'create'])->name('blog.create');
+    Route::post('/', [BlogController::class, 'store'])->name('blog.store');
+    Route::get('/{id}/edit', [BlogController::class, 'edit'])->name('blog.edit');
+    Route::put('/{id}', [BlogController::class, 'update'])->name('blog.update');
+    Route::delete('/{id}', [BlogController::class, 'destroy'])->name('blog.destroy');
+});
