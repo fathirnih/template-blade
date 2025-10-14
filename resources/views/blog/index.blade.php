62abd33c5a50e73e2af130ff5ee4ab8e99536bde
Fathir <fathirulfatih29@gmail.com>
2025-10-14 13:01:36 +0700

    add image to table blogs

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page - My Website</title>
+    <script src="{{ asset('blog/script.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('blog/style.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Inter:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
@@ -69,6 +70,6 @@
     <!-- Footer -->
     @include('blog.footer')
 
-    <script src="{{ asset('script.js') }}"></script>
+    <script src="{{ asset('blog/script.js') }}"></script>
 </body>
 </html>
