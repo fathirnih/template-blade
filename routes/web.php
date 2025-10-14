c8172dccc3538cec07f4a6f2d3814ebf04435cc7
Fathir <fathirulfatih29@gmail.com>
2025-10-14 10:40:40 +0700

    add content http method

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'welcome';
+});
+
+
+Route::post('/submit', function (){
+    return 'Hello';
+});
+
+Route::put('/update', function(){
+    return 'data berhasil di update';
+});
+
+Route::delete('/delete', function(){
+    return 'data berhasil di update';
+});
