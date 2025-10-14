4b2915616aad0786121ea0d4cb826c520916f60a
Fathir <fathirulfatih29@gmail.com>
2025-10-14 15:34:51 +0700

    change function update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -33,11 +33,25 @@ class BlogController extends Controller
 
  
     public function update(Request $request, string $id)
-    {
-          $blog = Blog::findOrFail($id);
-        $blog->update($request->all());
-        return redirect()->route('admin.dashboard')->with('success', 'Blog berhasil diperbarui');
+{
+    $blog = Blog::findOrFail($id);
+
+    // Jika ingin upload gambar baru
+    if($request->hasFile('image')){
+        $imageName = $request->file('image')->getClientOriginalName();
+        $request->file('image')->move(public_path('images'), $imageName);
+        $blog->image = $imageName;
     }
+
+    $blog->title = $request->title;
+    $blog->author = $request->author;
+    $blog->date = $request->date;
+    $blog->content = $request->content;
+
+    $blog->save();
+
+    return redirect()->route('admin.dashboard')->with('success', 'Artikel berhasil diperbarui');
+}
 
 
     public function destroy(string $id)
