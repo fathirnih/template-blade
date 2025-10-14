f308fd53a204842b1f36435e754ef4687bf168dd
Fathir <fathirulfatih29@gmail.com>
2025-10-14 13:29:02 +0700

    add content admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,16 +2,19 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'King Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+        ]);
     }
 }
