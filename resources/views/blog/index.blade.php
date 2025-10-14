71d75d31bc6ec048849a7bb6c243573f83ddd78d
Fathir <fathirulfatih29@gmail.com>
2025-10-14 13:44:08 +0700

    change jelajahi sekarang

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -29,7 +29,7 @@
 <section id="home" class="hero">
     <h1>Selamat Datang di Website Kami</h1>
     <p>Temukan berbagai artikel menarik dan informasi bermanfaat di sini.</p>
-    <a href="{{ route('blog.index') }}" class="cta-btn">Jelajahi Sekarang</a>
+    <a href="#article" class="cta-btn">Jelajahi Sekarang</a>
 </section>
 
 
