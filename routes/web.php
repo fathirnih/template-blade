72750ad42f9abf2aeb3b810e83d53b1e938e6d8d
Fathir <fathirulfatih29@gmail.com>
2025-10-15 13:49:13 +0700

    add prefix

--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,12 @@ Route::middleware(['admin'])->prefix('admin')->group(function () {
 
     Route::resource('blog', BlogController::class)->except(['show']);
 
+    //jika menggunakan prefix 
+    // CRUD Blog
+ //   Route::get('/create', [BlogController::class, 'create'])->name('blog.create');
+   // Route::post('/', [BlogController::class, 'store'])->name('blog.store');
+   // Route::get('/{id}/edit', [BlogController::class, 'edit'])->name('blog.edit');
+   // Route::put('/{id}', [BlogController::class, 'update'])->name('blog.update');
+   // Route::delete('/{id}', [BlogController::class, 'destroy'])->name('blog.destroy');
   
 });
