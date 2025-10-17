dec2c0dbc9c5f8975fa69d42f877e590163103eb
Fathir <fathirulfatih29@gmail.com>
2025-10-17 10:44:34 +0700

    add content about

---
+++ resources/views/blog/about.blade.php
@@ -0,0 +1,13 @@
+@extends('blog.layouts.master')
+
+@section('title', 'Tentang Kami')
+
+@section('content')
+<section id="about-us" class="about-section">
+    <h2>Tentang Kami</h2>
+    <p>
+        Kami adalah tim pengembang muda yang bersemangat untuk menghadirkan informasi menarik, 
+        edukatif, dan inspiratif untuk semua pembaca.
+    </p>
+</section>
+@endsection
