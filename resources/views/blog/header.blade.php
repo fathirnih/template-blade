9418b2dd51635fb10a3955c4999556bb91336249
Fathir <fathirulfatih29@gmail.com>
2025-10-17 10:27:30 +0700

    add content header

---
+++ resources/views/blog/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="logo">MyWebsite</div>
+    <nav>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#article">Article</a></li>
+            <li><a href="#about-us">About Us</a></li>
+        </ul>
+    </nav>
+    <button id="dark-mode-toggle">🌙</button>
+</header>
